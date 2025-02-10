69935238e0b04fbb6f798fa91f087bad18295dbb
Drake Olejniczak <drake.olejniczak@gmail.com>
2025-02-09 21:28:33 -0500

    Add stylesheets to example App

--- example/src/App.js
+++ example/src/App.js
@@ -11,6 +11,11 @@ function App() {
         <p>Your converted form will appear here</p>
       </header>
       <main>
+      <link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/stylebuilder/static/form-common.css?v=674b3f4
+      "/>
+      <link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?v=3.3.60851&themeRevisionID=5f30e2a790832f3e96009402"/>
+      <link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/css/styles/payment/payment_styles.css?3.3.60851" />
+      <link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/css/styles/payment/payment_feature.css?3.3.60851" />
         <FormComponent />
       </main>
     </div>
